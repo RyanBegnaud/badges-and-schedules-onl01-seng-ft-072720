8b67349ca6579456c817b1b8ec8763eda8f2d881
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-29 17:11:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,14 +12,13 @@ def batch_badge_creator(array)
   return new_array
 end
 
-counter = 1
-def assign_rooms(array, counter)
+index = 1
+def assign_rooms(array, index)
   room_array = []
   array.each do |name|
-    room_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter += 1
+    room_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
+    index += 1
   end
   return room_array
 end
 
-assign_rooms(array, counter)
